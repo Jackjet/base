86748eaf36757d9f0246495051f5c435a408da56
conan <yilinglin@xmisp.com>
2016-10-24 23:02:04 +0800

    验证码

--- Novots.Web/Controllers/LoginController.cs
+++ Novots.Web/Controllers/LoginController.cs
@@ -57,10 +57,10 @@ namespace Conan.Web.Controllers
             logEntity.F_Type = DbLogType.Login.ToString();
             try
             {
-                if (Session["Conan_session_verifycode"].IsEmpty() || Md5.md5(code.ToLower(), 16) != Session["Conan_session_verifycode"].ToString())
-                {
-                    throw new Exception("验证码错误，请重新输入");
-                }
+                ////if (Session["Conan_session_verifycode"].IsEmpty() || Md5.md5(code.ToLower(), 16) != Session["Conan_session_verifycode"].ToString())
+                ////{
+                ////    throw new Exception("验证码错误，请重新输入");
+                ////}
 
                 UserEntity userEntity = new UserApp().CheckLogin(username, password);
                 if (userEntity != null)
